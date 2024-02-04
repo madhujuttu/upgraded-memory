0a1f26f034b501ba802fe411aea899a8d175c4ba
madhujuttu <158794691+madhujuttu@users.noreply.github.com>
2024-02-04 11:06:24 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        float n1,n2;
+        System.out.print("Enter number 1: ");
+        n1=input.nextFloat();
+        System.out.println("Enter number 2: ");
+        n2=input.nextFloat();
+        int choice;
+        System.out.println("1.Addition,2.Substraction,3.Multiplication,4.Division.");
+        System.out.println("Enter any choice: ");
+        choice=input.nextInt();
+        if(choice==1){
+            System.out.println(n1+"+"+n2+"="+(n1+n2));
+            System.out.println("the output"+(n1+n2)+"is printed");
+        }
+        else if(choice==2)
+        {
+            System.out.println(n1+"-"+n2+"="+(n1-n2));
+            System.out.println("the output"+(n1-n2)+"is printed");
+        }
+        else if(choice==3){
+            System.out.println(n1+"*"+n2+"="+(n1*n2));
+            System.out.println("the output"+(n1*n2)+"is printed");
+        }
+        else if(choice==4){
+            System.out.println(n1+"/"+n2+"="+(n1/n2));
+            System.out.println("the output"+(n1/n2)+"is printed");
+        }
+        else{
+            System.out.println("you have entered wrong choice please check the above menu and continue!!");
+        }
+        input.close();
+    }
+}
