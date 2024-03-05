4ab01c19545692e9268071995c1c8602612d7ddc
madhujuttu <158794691+madhujuttu@users.noreply.github.com>
2024-03-05 19:09:13 +0530

    Create Diamond.cpp

---
+++ Diamond.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+class Creature {
+public:
+    void identify() {
+        cout << "I am a Creature." << endl;
+    }
+};
+class FlyingCreature : virtual public Creature {
+public:
+    void fly() {
+        cout << "I can fly!" << endl;
+    }
+};
+
+class RunningCreature : virtual public Creature {
+public:
+    void run() {
+        cout << "I can run!" << endl;
+    }
+};
+class FlyingMammal : public FlyingCreature, public RunningCreature {
+public:
+    void greet() {
+        cout << "I am a Flying Mammal." << endl;
+    }
+};
+int main() {
+    FlyingMammal bat;
+    bat.greet(); 
+ bat.identify(); 
+    bat.run();
+    bat.fly();
+    return 0;
+}
